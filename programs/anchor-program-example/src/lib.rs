9b938bb186aee1ca64e1c4c914f5de0921c992cf
codemaster1001 <blockchaincrazy1001@gmail.com>
2024-06-01 20:36:06 +0200

    couter program

--- programs/anchor-program-example/src/lib.rs
+++ programs/anchor-program-example/src/lib.rs
@@ -5,64 +5,41 @@ use anchor_lang::prelude::*;
 declare_id!("FhjBVpnwE8R6wBLS1X2YEfTXzjednen5MDFUDoeoiJG7");
 
 #[program]
-pub mod anchor_program_example {
+pub mod counter_anchor {
     use super::*;
 
-    pub fn create_user(ctx: Context<CreateUserContext>, name: String) -> Result<()> {
-        *ctx.accounts.user_account = UserState {
-            bump: ctx.bumps.user_account,
-            user: ctx.accounts.user.key(),
-            name,
-        };
+    pub fn initialize_counter(_ctx: Context<InitializeCounter>) -> Result<()> {
         Ok(())
     }
-    
-    pub fn close_user(_ctx: Context<CloseUserContext>) -> Result<()> {
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CloseUserContext<'info> {
+pub struct InitializeCounter<'info> {
     #[account(mut)]
-    pub user: Signer<'info>,
-
-    #[account(
-        mut,
-        seeds = [
-            b"USER",
-            user.key().as_ref(),
-        ],
-        bump = user_account.bump,
-        close = user, // close account and return lamports to user
-    )]
-    pub user_account: Account<'info, UserState>,
-}
-
-#[derive(Accounts)]
-pub struct CreateUserContext<'info> {
-    #[account(mut)]
-    pub user: Signer<'info>,
+    pub payer: Signer<'info>,
 
     #[account(
         init,
-        payer = user,
-        space = UserState::INIT_SPACE,
-        seeds = [
-            b"USER",
-            user.key().as_ref(),
-        ],
-        bump
+        space = 8 + Counter::INIT_SPACE,
+        payer = payer
     )]
-    pub user_account: Account<'info, UserState>,
+    pub counter: Account<'info, Counter>,
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
 #[account]
-#[derive(InitSpace)] // automatically calculate the space required for the struct
-pub struct UserState {
-    pub bump: u8,     // 1 byte
-    pub user: Pubkey, // 32 bytes
-    #[max_len(50)] // set a max length for the string
-    pub name: String, // 4 bytes + 50 bytes
+#[derive(InitSpace)]
+pub struct Counter {
+    count: u64,
 }
--- tests/anchor-program-example.ts
+++ tests/anchor-program-example.ts
@@ -1,52 +1,63 @@
 import * as anchor from "@coral-xyz/anchor"
-import { AnchorProgramExample } from "../target/types/anchor_program_example"
+import { CounterAnchor } from "../target/types/counter_anchor"
 import { Program } from "@coral-xyz/anchor"
-import { PublicKey } from "@solana/web3.js"
-import assert from "assert"
+import { Keypair } from "@solana/web3.js"
+import { assert } from "chai"
 
-describe("close-an-account", () => {
+describe("counter_anchor", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider)
-
-  const program = anchor.workspace
-    .AnchorProgramExample as Program<AnchorProgramExample>
   const payer = provider.wallet as anchor.Wallet
 
-  // Derive the PDA for the user's account.
-  const [userAccountAddress] = PublicKey.findProgramAddressSync(
-    [Buffer.from("USER"), payer.publicKey.toBuffer()],
-    program.programId
-  )
+  const program = anchor.workspace.CounterAnchor as Program<CounterAnchor>
 
-  it("Create Account", async () => {
+  // Generate a new keypair for the counter account
+  const counterKeypair = new Keypair()
+
+  it("Initialize Counter", async () => {
     await program.methods
-      .createUser("John Doe")
+      .initializeCounter()
       .accounts({
-        user: payer.publicKey,
+        counter: counterKeypair.publicKey,
+        payer: payer.publicKey,
       })
+      .signers([counterKeypair])
       .rpc()
 
-    // Fetch the account data
-    const userAccount = await program.account.userState.fetch(
-      userAccountAddress
+    const currentCount = await program.account.counter.fetch(
+      counterKeypair.publicKey
     )
-    assert.equal(userAccount.name, "John Doe")
-    assert.equal(userAccount.user.toBase58(), payer.publicKey.toBase58())
+
+    assert(
+      currentCount.count.toNumber() === 0,
+      "Expected initialized count to be 0"
+    )
   })
 
-  it("Close Account", async () => {
+  it("Increment Counter", async () => {
     await program.methods
-      .closeUser()
-      .accounts({
-        user: payer.publicKey,
-      })
+      .increment()
+      .accounts({ counter: counterKeypair.publicKey })
       .rpc()
 
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.userState.fetchNullable(
-      userAccountAddress
+    const currentCount = await program.account.counter.fetch(
+      counterKeypair.publicKey
     )
-    assert.equal(userAccount, null)
+
+    assert(currentCount.count.toNumber() === 1, "Expected  count to be 1")
+  })
+
+  it("Increment Counter Again", async () => {
+    await program.methods
+      .increment()
+      .accounts({ counter: counterKeypair.publicKey })
+      .rpc()
+
+    const currentCount = await program.account.counter.fetch(
+      counterKeypair.publicKey
+    )
+
+    assert(currentCount.count.toNumber() === 2, "Expected  count to be 2")
   })
 })
